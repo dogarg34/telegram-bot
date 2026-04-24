b2e19a0f35f2bd38a36965daf3e57dadc60fbce5
dogarg34 <kingdogar2222@gmail.com>
2026-04-24 08:56:52 +0200

    Refactor country data handling in config.py
    Removed hardcoded countries and updated to fetch from iVasms.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+# Railway variables se read karega
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 ADMIN_ID = int(os.environ.get('ADMIN_ID', 0))
 IVASMS_EMAIL = os.environ.get('IVASMS_EMAIL')
@@ -7,9 +8,5 @@ IVASMS_PASSWORD = os.environ.get('IVASMS_PASSWORD')
 GROUP_ID = int(os.environ.get('GROUP_ID', 0))
 AUTO_REFRESH_INTERVAL = int(os.environ.get('AUTO_REFRESH', 10800))
 
-COUNTRIES = {
-    "Pakistan": "+92",
-    "Bangladesh": "+880",
-    "Indonesia": "+62",
-    "icovey coast": "225",
-}
+# Ye ab hardcoded nahi rahenge - bot auto-detect karega
+# COUNTRIES ab iVasms se auto fetch honge
